afc64d595b49e19df6d8878dd00062e9050d085b
ORYXCACHEIRO <55361488+ORYXCACHEIRO@users.noreply.github.com>
2021-12-01 19:41:04 +0000

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
-
+import { Popover,Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
 function Header() {
   
   return (
     <>
+    
     <div className="m-3 ml-4 mr-4 rounded-t-2xl flex bg-gray-900 h-12 ">
-      <span className="text-white font-bold pt-2 pl-5">Follow</span>
+      <span className="text-white font-bold pt-2 pl-5">Follow Us</span>
       <div className="pt-2 pl-5 text-white font-bold  space-x-4">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6  inline-block" viewBox="0 0 97.75 97.75" fill="currentColor">
           <path d="M48.875,0C21.882,0,0,21.882,0,48.875S21.882,97.75,48.875,97.75S97.75,75.868,97.75,48.875S75.868,0,48.875,0zM67.521,24.89l-6.76,0.003c-5.301,0-6.326,2.519-6.326,6.215v8.15h12.641L67.07,52.023H54.436v32.758H41.251V52.023H30.229V39.258h11.022v-9.414c0-10.925,6.675-16.875,16.42-16.875l9.851,0.015V24.89L67.521,24.89z" />
@@ -16,41 +18,103 @@ function Header() {
         </svg>
       </div>
     </div>
-    <div className="m-3 ml-4 mr-4 mb-10 rounded-b-3xl flex bg-gradient-to-r from-purple-600 to-purple-400 z-10 bottom-4 relative">
-        <div className="p-px  w-1/6 md:w-5/6">
-            <h1 className="text-white m-5 ml-8 font-extrabold text-5xl">LOGO.</h1>
-        </div>
-        <div className="w-4/6 flex md:hidden">
-          <div className="w-1/12"></div>
-          <div className="w-10/12 flex">
-            <div className="w-full flex">
-              <div className="m-auto ml-6 mb-9 lg:mb-10">
-                <label className="font-bold text-lg text-white">Search</label>
-              </div>
-              <input type="text" placeholder="hotel..." className="border-3 border-transparent rounded-3xl h-10 mt-6 w-4/5 ml-4 pl-3 focus:outline-none focus:ring-3 focus:ring-purple-700 focus:border-transparent" />
-              <button className="shadow-lg transition duration-500 ease-in-out bg-purple-900 font-bold text-lg p-3 pl-4 pr-4 rounded-3xl text-white m-5 hover:bg-gray-700 lg:mb-6">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-              </button>
+    <div className="m-3 ml-4 mr-4 mb-10 rounded-b-3xl flex bg-gradient-to-r from-purple-600 to-purple-400 z-10 bottom-4 relative">  
+      <div className="p-px  w-1/6 md:w-5/6">
+          <h1 className="text-white m-5 ml-8 font-extrabold text-5xl">LOGO.</h1>
+      </div>
+      <div className="w-4/6 flex md:hidden">
+        <div className="w-1/12"></div>
+        <div className="w-10/12 flex">
+          <div className="w-full flex">
+            <div className="m-auto ml-6 mb-9 lg:mb-10">
+              <label className="font-bold text-lg text-white">Search</label>
             </div>
-          </div>
-        </div>
-        <div className="w-1/6 ">
-          <div className="md:hidden flex ">
-            <button type="button" className="mr-auto ml-auto shadow-lg transition duration-500 ease-in-out bg-gray-900 font-bold text-lg p-3 pl-4 pr-4 rounded-3xl text-white m-5 space-x-2 hover:bg-gray-700">
-            <span className="inline-block lg:text-sm lg:mb-2">
-              Sign In
-            </span>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block mb-1 lg:text-sm lg:mb-0" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg> 
+            <input type="text" placeholder="hotel..." className="border-3 border-transparent rounded-3xl h-10 mt-6 w-4/5 ml-4 pl-3 focus:outline-none focus:ring-3 focus:ring-purple-700 focus:border-transparent" />
+            <button className="shadow-lg transition duration-300 ease-in-out bg-purple-900 font-bold text-lg p-3 pl-4 pr-4 rounded-3xl text-white m-5 hover:bg-gray-700 lg:mb-6">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
             </button>
           </div>
-          <div className="md:contents hidden">
-          </div>
         </div>
+      </div>
+      <div className="w-1/6">
+        <div className="md:hidden flex ">
+          <button type="button" className="mr-auto ml-auto shadow-lg transition duration-300 ease-in-out bg-gray-900 font-bold text-lg p-3 pl-4 pr-4 rounded-3xl text-white m-5 space-x-2 hover:bg-gray-700">
+          <span className="inline-block lg:text-sm lg:mb-2">
+            Sign In
+          </span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block mb-1 lg:text-sm lg:mb-0" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
+          </svg> 
+          </button>
+        </div>
+        <div className=" md:contents hidden ">
+          <Popover>
+
+          {({ open }) => ( 
+            <>
+              <Popover.Button className=" mr-auto ml-auto block m-5 rounded-3xl bg-purple-900 text-white hover:bg-gray-700 shadow-lg transition duration-500 ease-in-out sm:mr-5">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-9 w-9 m-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                </svg>
+              </Popover.Button>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-in duration-250"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-out duration-250"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+
+                <Popover.Panel className="border-3 border-purple-600  absolute z-30 w-full left-0 top-0 bg-white h-80 rounded-b-2xl">
+                  <div className="relative">
+                    <div className="border-3 border-transparent">
+                      <Popover.Button onClick={() => (open = false)} className=" absolute  font-bold right-4 top-2 rounded-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                      </svg>
+                      </Popover.Button>
+                    </div>
+                    <div>
+                      <h1 className="text-purple-600 m-4 ml-8 font-extrabold text-5xl text-center">LOGO.</h1>
+                    </div>
+                    <div className="flex">
+                      <input type="text" placeholder="hotel..." className="border-3 border-purple-700 rounded-3xl h-10 mt-6 w-4/5 ml-10 pl-3 focus:outline-none" />
+                      <button className="shadow-lg transition duration-300 ease-in-out bg-purple-900 font-bold text-lg p-3 pl-4 pr-4 rounded-3xl text-white m-5 hover:bg-gray-700 lg:mb-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                      </button>
+                    </div>
+                    <div className="">
+                      <button className="bg-purple-700 text-white mr-auto ml-auto block  px-40 m-4 p-3  text-lg font-medium rounded-full">
+                        Sign Up
+                      </button>
+                    </div>
+                  </div>
+                </Popover.Panel>
+
+                </Transition>
+
+                <Popover.Overlay
+                  className={`${
+                    open ? 'opacity-30 fixed inset-0' : 'opacity-0'
+                  } bg-black`}
+                />
+            </>
+            )}
+          </Popover>
+        </div>
+      </div>
     </div>
+
+    
+
+    
 
     </>
   );
