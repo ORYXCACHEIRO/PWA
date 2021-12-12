46ef7f7512efd88da8ba57f2782b61e4c1f9d3d1
Silanthris <47530239+Silanthris@users.noreply.github.com>
2021-12-12 17:38:40 +0000

    Footer, Ja tou a ficar farto disto e mal começei o suicidio por ves e msm a melhor soluçao
    Gofada se a culpa e toda do Vieira

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import logo from './logo.svg';
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
       
   );
