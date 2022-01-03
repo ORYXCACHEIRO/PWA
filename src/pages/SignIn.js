0c16f2dd2e3bbb6bac72fa7f73212311343364b2
Silanthris <47530239+Silanthris@users.noreply.github.com>
2022-01-03 20:48:47 +0000

    dsa
    dasd

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,24 +1,44 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignIn = () => {
 
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
 
-    return(
+    const [loginSuccess, setLoginSuccess] = useState(false);
+    const submit = data => login(data);
+
+    const login = (data) => {
+        fetch('http://127.0.0.1:3000/auth/login', {
+            headers: {'Content-type': 'application/json'},
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then( r => r.json())
+        .then((response) => {
+            console.log(response);
+            if(response.auth){
+                setLoginSuccess(true);
+            } else {
+                alert('Login Errado');
+            }
+        });
+    }
+
+    return (
         <>
             <div className="">
                 <div className="p-8">
                     <h1 className=" text-center text-5xl  font-bold">Sign in.</h1>
                     <div className="mt-10 flex justify-center">
-                        <form className="w-80 flex flex-col gap-10">
+                        <form className="w-80 flex flex-col gap-10" onSubmit={handleSubmit(submit)} >
                             <div className=" flex flex-col gap-2">
                                 <label className="font-medium text-xl">Email:</label>
-                                <input type="email" {...register('email', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1' placeholder='email@email.com'/>
+                                <input type="email" {...register('email', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1' placeholder='email@email.com' />
                             </div>
                             <div className=" flex flex-col gap-2">
                                 <label className="font-medium text-xl">Password:</label>
-                                <input type="password" {...register('password', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1 ' placeholder='*********'/>
+                                <input type="password" {...register('password', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1 ' placeholder='*********' />
                             </div>
                             <button type="submit" className='bg-purple-500 text-white font-medium p-3 rounded-lg transition duration-100 ease-out hover:ease-in hover:bg-purple-300'>
                                 Sign In
