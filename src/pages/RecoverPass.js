811300aa3127392e09cea1dd83fc5c454431b12e
Silanthris <47530239+Silanthris@users.noreply.github.com>
2022-01-06 23:57:15 +0000

    R F
    Recover  Fix

--- src/pages/RecoverPass.js
+++ src/pages/RecoverPass.js
@@ -1,33 +1,32 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 const ForgotPass = () => {
 
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
     const submit = data => recoverPass(data);
 
-    const url = window.location.href 
-    let key = null;
-    if(url)
-    {
-       key = url.split('/')[5];
-       console.log(key)
-    }
+    const url = window.location.href
+
+    console.log(url)
+    let key = "";
+    key = url.split('/')[4];
+    console.log(key)
 
 
     const recoverPass = (data) => {
         fetch(`/auth/recover/${key}`, {
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             method: 'POST',
             body: JSON.stringify(data)
         })
-        .then((response) => {
-            console.log(response)
-        });
+            .then((response) => {
+                console.log(response)
+            });
     }
 
 
-   
-    return(
+
+    return (
         <>
             <div className="">
                 <div className=" p-28">
@@ -36,7 +35,7 @@ const ForgotPass = () => {
                         <form className="w-80 flex flex-col gap-10" onSubmit={handleSubmit(submit)} >
                             <div className=" flex flex-col gap-2">
                                 <label className="font-medium text-xl">New Password:</label>
-                                <input type="password" {...register('password', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1' placeholder=''/>
+                                <input type="password" {...register('password', { required: true })} className='border-3 border-purple-500 rounded-lg pl-2 p-1' placeholder='' />
                             </div>
                             <button type="submit" className='bg-purple-500 text-white font-medium p-3 rounded-lg transition duration-100 ease-out hover:ease-in hover:bg-purple-300'>
                                 Send
